102c3eceb7f05a6989f333f6174888ce8d56d70a
fariz <farizi1906@gmail.com>
2022-01-15 09:24:36 +0700

    add some meta tags

--- client/MainView.js
+++ client/MainView.js
@@ -1,9 +1,12 @@
 import m from "mithril"
+import $ from "jquery"
 import { Nav, Footer, theme } from "./Nav"
 import { Form } from "./Form"
 
 const Home = {
   view: function () {
+  	$("head").append(`<meta property="og:title" content="Paste bin clone app" />`)
+  	$("head").append(`<meta property="og:url" content="https://pastebin-clone.netlify.app/#!/newpaste" />`)
     document.title = "New paste"
     return m(".container",
       m(Nav),
